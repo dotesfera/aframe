465f2ad629d22dd02aac2216705e5117d4162cfc
Joao <joaomelo17@gmail.com>
2020-05-25 11:46:58 +0100

    Removed console logs

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -1,13 +1,13 @@
-var debugLib = require('debug');
-var extend = require('object-assign');
+var debugLib = require("debug");
+var extend = require("object-assign");
 
 var settings = {
   colors: {
-    debug: 'gray',
-    error: 'red',
-    info: 'gray',
-    warn: 'orange'
-  }
+    debug: "gray",
+    error: "red",
+    info: "gray",
+    warn: "orange",
+  },
 };
 
 /**
@@ -20,7 +20,9 @@ var debug = function (namespace) {
 
   d.color = getDebugNamespaceColor(namespace);
 
-  return d;
+  // return d;
+  // Damos funcao vazia para nao mostrar o log
+  return function () {};
 };
 extend(debug, debugLib);
 
@@ -32,10 +34,10 @@ extend(debug, debugLib);
  * @returns {String} The type of the namespace (e.g., `warn`).
  * @api private
  */
-function getDebugNamespaceType (namespace) {
-  var chunks = namespace.split(':');
+function getDebugNamespaceType(namespace) {
+  var chunks = namespace.split(":");
 
-  return chunks[chunks.length - 1];  // Return the last one
+  return chunks[chunks.length - 1]; // Return the last one
 }
 
 /**
@@ -46,7 +48,7 @@ function getDebugNamespaceType (namespace) {
  * @returns {String} The color of the namespace (e.g., `orange`).
  * @api private
  */
-function getDebugNamespaceColor (namespace) {
+function getDebugNamespaceColor(namespace) {
   var type = getDebugNamespaceType(namespace);
 
   var color = settings.colors && settings.colors[type];
@@ -63,11 +65,10 @@ function getDebugNamespaceColor (namespace) {
  * @returns {localStorage}
  * @api private
  */
-function storage () {
+function storage() {
   try {
     return window.localStorage;
-  } catch (e) {
-  }
+  } catch (e) {}
 }
 
 /**
@@ -81,12 +82,14 @@ function storage () {
  *
  */
 var ls = storage();
-if (ls && (parseInt(ls.logs, 10) || ls.logs === 'true')) {
-  debug.enable('*');
+if (ls && (parseInt(ls.logs, 10) || ls.logs === "true")) {
+  debug.enable("*");
 } else {
-  debug.enable('*:error,*:info,*:warn');
+  debug.enable("*:error,*:info,*:warn");
 }
 
-if (process.browser) { window.logs = debug; }
+if (process.browser) {
+  window.logs = debug;
+}
 
 module.exports = debug;
